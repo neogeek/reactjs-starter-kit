2db87378e2f41d7f704cb2f70cb8ca6fe1ed51b8
Scott Doxey <neogeek0101@gmail.com>
2017-03-14 19:56:14 -0400

    Added no match route.

--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 import AboutView from './views/about';
 import AppView from './views/app';
+import NoMatchView from './views/nomatch';
 import WelcomeView from './views/welcome';
 
 export {
@@ -14,6 +15,9 @@ export {
 };
 
 export default <AppView>
-    <Route exact path="/" component={WelcomeView} />
-    <Route path="/about" component={AboutView} />
+    <Switch>
+        <Route exact path="/" component={WelcomeView} />
+        <Route path="/about" component={AboutView} />
+        <Route component={NoMatchView} />
+    </Switch>
 </AppView>;
---
+++ src/js/views/nomatch.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NoMatchView = React.createClass({
+
+    render () {
+
+        return (
+            <p>No Match</p>
+        );
+
+    }
+
+});
+
+export default NoMatchView;
