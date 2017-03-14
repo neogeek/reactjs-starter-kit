ae1a5b7898687900345ba466a70034e437b366d9
Scott Doxey <neogeek0101@gmail.com>
2017-03-14 19:35:45 -0400

    Updated server side code.

--- src/server.jsx
+++ src/server.jsx
@@ -2,7 +2,7 @@ import path from 'path';
 
 import React, {createElement} from 'react';
 import {renderToString} from 'react-dom/server';
-import {match, RouterContext} from 'react-router';
+import {StaticRouter} from 'react-router';
 import routes from './js/routes';
 
 import express from 'express';
@@ -16,7 +16,11 @@ app.use(express.static(path.join(__dirname, '../static')));
 
 app.get('*', (req, res) => {
 
-    res.status(200).render('default', {'content': ''});
+    res.status(200).render('default', {
+        'content': renderToString(<StaticRouter context={{}} location={req.url}>
+            {routes}
+        </StaticRouter>)
+    });
 
 });
 
