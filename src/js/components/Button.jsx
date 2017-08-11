d9e8ecc4531132ab9e5ddfac6681d820aa66edb3
Scott Doxey <neogeek0101@gmail.com>
2017-08-11 19:18:22 -0400

    Added new sample component.

---
+++ src/js/components/Button.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+import PropTypes from 'prop-types';
+
+class Button extends Component {
+
+    state = {
+        'label': 'Hello, Button!'
+    };
+
+    static propTypes = {
+        'href': PropTypes.string.isRequired,
+        'label': PropTypes.string.isRequired
+    };
+
+    constructor (props) {
+
+        super(props);
+
+        this.state = {
+            'href': props.href,
+            'label': props.label
+        };
+
+    }
+
+    render () {
+
+        return (
+            <a href={this.state.href} className="button">{this.state.label}</a>
+        );
+
+    }
+
+}
+
+export default Button;
