afac9b51ea8beaab938c06aa587271bde5472806
Scott Doxey <neogeek0101@gmail.com>
2017-08-11 19:17:03 -0400

    Removed unused exported views.

--- src/js/routes.jsx
+++ src/js/routes.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom';
 
 import {Route, Switch} from 'react-router-dom';
 
-import AboutView from './views/about';
 import AppView from './views/app';
 import NoMatchView from './views/nomatch';
-import WelcomeView from './views/welcome';
 
-export {
-    AboutView,
-    AppView,
-    WelcomeView
-};
+import AboutView from './views/about';
+import WelcomeView from './views/welcome';
 
 export default <AppView>
     <Switch>
