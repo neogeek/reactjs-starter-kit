c004e36545752fcd1618d5c3b927cb657d65bd0b
Scott Doxey <neogeek0101@gmail.com>
2017-08-11 19:18:34 -0400

    Added sample component to welcome view.

--- src/js/views/welcome.jsx
+++ src/js/views/welcome.jsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 
+import Button from '../components/Button';
+
 class WelcomeView extends Component {
 
     render () {
 
         return (
-            <p>Welcome!</p>
+            <p>Welcome to <Button href="https://github.com/neogeek/reactjs-starter-kit" label="reactjs-starter-kit" />! A starter-kit built with ❤️ by  <Button href="https://github.com/neogeek/" label="@neogeek" />!</p>
         );
 
     }
