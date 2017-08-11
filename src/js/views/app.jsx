e94da6609d9d8adb1d15f1875d49ba6b741647e2
Scott Doxey <neogeek0101@gmail.com>
2017-08-11 19:16:30 -0400

    Fixed invalid use of React.Component.

--- src/js/views/app.jsx
+++ src/js/views/app.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import {Link} from 'react-router-dom';
 
-class AppView extends React.Component {
+class AppView extends Component {
 
     render () {
 
